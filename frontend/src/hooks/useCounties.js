431eaaabf48343899dca5455dcae5fadb772cb47
Tanner Keyes <keyescode@gmail.com>
2024-02-25 11:08:49 -0500

    Added hook to pull counties and pass data in string

---
+++ frontend/src/hooks/useCounties.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export const useCounties = () => {
+  const [counties, setCounties] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = `https://0d5948b2723b.ngrok.app/counties`;
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setCounties(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { counties, loading, error };
+};
