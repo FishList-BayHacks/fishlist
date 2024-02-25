c219d00166354984f9a69a4ac111585379422b6e
John Aagaard <47125152+jaagaard01@users.noreply.github.com>
2024-02-25 12:15:41 -0500

    set up store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,25 +4,28 @@ import AboutPage from "./pages/AboutPage";
 import DiveSiteFishPage from "./pages/DiveSiteFishPage";
 import DiveSitesPage from "./pages/DiveSitesPage";
 import HomePage from "./pages/HomePage";
+import { MainProvider } from "./context/Context";
 import NotFoundPage from "./pages/NotFoundPage";
 import ThemeProvider from "./providers/theme-provider";
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-          <Route path="/divesites/:county" element={<DiveSitesPage />} />
-          <Route
-            path="/divesite-fish/:county/:siteId"
-            element={<DiveSiteFishPage />}
-          />
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <MainProvider>
+      <ThemeProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+            <Route path="/divesites/:county" element={<DiveSitesPage />} />
+            <Route
+              path="/divesite-fish/:county/:siteId"
+              element={<DiveSiteFishPage />}
+            />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </MainProvider>
   );
 }
 
