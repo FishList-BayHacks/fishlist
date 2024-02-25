844d514113a64490f823d4a5e72f27a0c6591e9b
John Aagaard <47125152+jaagaard01@users.noreply.github.com>
2024-02-25 13:17:56 -0500

    context update

--- frontend/src/context/Context.jsx
+++ frontend/src/context/Context.jsx
@@ -6,10 +6,23 @@ export const Context = createContext();
 export const MainProvider = ({ children }) => {
   const [counties, setCounties] = useState([]);
   const [fish, setFish] = useState([]);
+  const [usersSeenList, setUsersSeenList] = useState([]);
 
   // Function to update counties state
   const updateCounties = (newCounties) => {
     setCounties(newCounties);
+  };
+
+  const initializeUserSeenFishList = (arrayOfFishIds) => {
+    setUsersSeenList(arrayOfFishIds);
+  };
+
+  const addFishIdToSeenList = (idToAdd) => {
+    setUsersSeenList([...usersSeenList, idToAdd]);
+  };
+
+  const removeFishIdOnSeenList = (idToRemove) => {
+    setUsersSeenList(usersSeenList.filter((id) => id !== idToRemove));
   };
 
   // Function to update fish state
@@ -17,7 +30,17 @@ export const MainProvider = ({ children }) => {
     setFish(newFish);
   };
   return (
-    <Context.Provider value={{ counties, fish, updateCounties, updateFish }}>
+    <Context.Provider
+      value={{
+        counties,
+        fish,
+        updateCounties,
+        updateFish,
+        initializeUserSeenFishList,
+        addFishIdToSeenList,
+        removeFishIdOnSeenList,
+      }}
+    >
       {children}
     </Context.Provider>
   );
