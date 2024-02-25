729274452055d5b1cc9c74ac9ac94462d7c2726a
Appboy <appboy@Appboys-MacBook-Pro.local>
2024-02-25 09:19:41 -0500

    Updating service call

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CountieService } from './service/countie.service';
+import { Counties } from './entities/counties.entity';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly countieService: CountieService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/counties")
+  async getCounties(): Promise<Counties[]> {
+    return await this.countieService.findAll();
   }
 }
